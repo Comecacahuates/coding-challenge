31373d77766f0663284f99b19b8ff3680433242e
Adrián Juárez <comecacahuates@yahoo.com>
2021-10-05 17:56:31 -0500

    Implement numberToOrdinal function
    Tests passed

--- src/utils.ts
+++ src/utils.ts
@@ -32,7 +32,53 @@ export function maskify(cardNumber: string) {
  * @param n Number to convert
  */
 export function numberToOrdinal(n: number) {
-  throw new Error("Not implemented");
+  if (n < 0) {
+    throw new InvalidArgumentError(
+      "Argument has to be greater than or equal to 0",
+    );
+  }
+
+  // Patterns for ordinal numbers
+  const ordinals = [
+    {
+      pattern: /^0$/, // 0
+      suffix: "",
+    },
+    {
+      pattern: /^(\d*[02-9])?1$/, // 1, 21, 31, ..., 101, ...
+      suffix: "st",
+    },
+    {
+      pattern: /^(\d*[02-9])?2$/, // 2, 22, 32, ..., 102, ...
+      suffix: "nd",
+    },
+    {
+      pattern: /^(\d*[02-9])?3$/, // 3, 23, 33, ..., 103, ...
+      suffix: "rd",
+    },
+    {
+      pattern: /\d+/, // Rest of the numbers
+      suffix: "th",
+    },
+  ];
+
+  // Keep only the integer part
+  n = parseInt(n.toString());
+
+  // Test number against each pattern
+  const suffix = ordinals.reduce((suffix: string | null, ordinal) => {
+    // If number already matched a pattern, return the same suffix
+    if (suffix !== null) {
+      return suffix;
+    }
+    // If number matches the pattern, return the correspondig suffix
+    if (ordinal.pattern.test(n.toString())) {
+      return ordinal.suffix;
+    }
+    return null;
+  }, null);
+
+  return `${n}${suffix}`;
 }
 
 /**
